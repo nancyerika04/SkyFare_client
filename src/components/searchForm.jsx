80327d8cfd7bf35eb1c305f659874b287d13ebb5
nancyerika04 <nancywouegam@gmail.com>
2025-08-14 21:08:03 +0100

    styling searchfrom

--- src/components/searchForm.jsx
+++ src/components/searchForm.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function searchForm() {
+  const[from, setFrom] = useState('');
+  const[to, setTo] = useState('');
+  const[depart, setDepart] = useState('');
+  const[returndate, setReturndate] = useState('');
+  const[flightclass, setFlightclass] = useState('');
+  const handleSubmit =(e) =>{
+  e.preventDefault();
+
+  const searchdata = {
+    from, to, depart, returndate, flightclass
+  };
+  console.log(searchdata);
+}
   return (
-    <div>
-      
+    <div className='bg-white border m-8 w-100 flex justify-center items-center px-4 shadow-2xl'>
+      <form onSubmit={handleSubmit} className='p-6 '>
+        <h2 className='text-2xl text-center font-bold mb-6 '>Search flights</h2>
+
+        <div className='mb-4'>
+          <label>From:</label>
+          <input type="text" placeholder="City" value={from} onChange={(e)=>setFrom(e.target.value)} className='border rounded p-2 mt-1'></input>
+        </div>
+
+        <div className='mb-4'>
+          <label>To:</label>
+          <input type="text" placeholder="City" value={to} onChange={(e)=>setTo(e.target.value)} className='border rounded p-2 mt-1'></input>
+        </div>
+
+        <div >
+          <label>Depart:</label>
+          <input type="date" value={depart} onChange={(e)=>setDepart(e.target.value)} className='border rounded p-2 mt-1'></input>
+        </div>
+
+        <div>
+          <label>Return:</label>
+          <input type="date" value={returndate} onChange={(e)=>setReturndate(e.target.value)} className='border rounded p-2 mt-1'></input>
+        </div>
+
+        <div>
+          <label>Flight class:</label>
+          <select value={flightclass} onChange={(e)=>setFlightclass(e.target.value)} className='border rounded p-2 mt-1'>
+            <option> Economy </option>
+            <option> Premium </option>
+            <option> Bussiness </option>
+            <option> First </option>
+          </select>
+        </div>
+
+        <button type="submit" className='bg-blue-600 rounded hover:bg-blue-300 p-1.5 items-center'>Search</button>
+      </form>
+
     </div>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import SearchForm from '../components/searchForm'
 
 export default function Home() {
   return (
     <div>
-      <h1>Find the best flights with real-time deals, AI-powered recommendations, and instant alerts.</h1>
+      <h1 className='font-semibold font-mono text-3xl justify-center'>Find smarter, cheaper flights—fast</h1>
+      <h2 className='text-2xl'>AI-powered deals, real-time alerts, and seamless booking </h2>
+      <p>[ Trust badges:  ★4.8  |  Secure  |  1M+ searches ]</p>
+      <SearchForm/>
+      <p> [⚡ Smart Deals]   [🔔 Price Alerts]   [🧠 AI Assistant]   [🛡️ Secure Checkout] </p>
     </div>
   )
 }
