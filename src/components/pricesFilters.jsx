6da9ee85a8a96453532cffb586ca11537ac7c2b4
nancyerika04 <nancywouegam@gmail.com>
2025-08-17 19:55:15 +0100

    styling prices & filters component

--- src/components/pricesFilters.jsx
+++ src/components/pricesFilters.jsx
@@ -2,36 +2,34 @@ import React from 'react'
 
 export default function pricesFilters() {
   return (
-    <div className='p-5 rounded-lg shadow-md w-full max-w-md border-blue-200 border'>
-      <form className='bg-gray-100 items-center '>
-
-        <h2 className='text-lg font-bold mb-4'>PRICES & FILTERS</h2>
-
-        <div className='mb-4'>
-            <label className='text-gray-500'>Prices:</label>
-            <input type="number" placeholder='$---$'/>
+    <div className='p-5 rounded-2xl shadow-2xl justify-center w-full items-center'>
+      <h2 className='text-lg font-bold mb-4 text-gray-700 text-center'>PRICES & FILTERS</h2>
+      <form className='bg-white items-center flex justify-center'>
+        <div className='m-5 mb-4'>
+            <label className='text-gray-500 mb-2'>Prices: </label>
+            <input className='border rounded-md' type="number" placeholder='$'/>
         </div>
         
-        <div className='mb-4'>
-            <label className='text-gray-500'>Stops:</label>
-            <select className=''>
+        <div className='m-5 mb-4'>
+            <label className='text-gray-500'>Stops: </label>
+            <select className='border rounded-md'>
                 <option>0</option>
                 <option>1</option>
                 <option>2+</option>
             </select>
         </div>
         
-        <div className='mb-4'>
-            <label className='text-gray-500'>Airlines:</label>
-            <select>
+        <div className='m-5 mb-4'>
+            <label className='text-gray-500'>Airlines: </label>
+            <select className='border rounded-md'>
                 <option>AirX</option>
                 <option>Skyjet</option>
             </select> 
         </div>
 
-        <div className='mb-4'> 
-            <label className='text-gray-500'>Sort:</label>
-            <select>
+        <div className='m-4 mb-4'> 
+            <label className='text-gray-500'>Sort: </label>
+            <select className='border rounded-md'>
                 <option value="">Price</option>
             </select>
         </div>
